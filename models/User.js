39bada7ff658277c1a1c22abe892536208fa6e20
Conor Donnelly <donn0021@umn.edu>
2023-01-17 17:58:26 -0600

    fixing User model to accomodate friends array

--- models/User.js
+++ models/User.js
@@ -16,12 +16,15 @@ const UserSchema = new Schema(
       match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
 
     },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
     thoughts: {
       thoughtsID: [],
     },
-    friends: {
-      friendsID: [],
-    }
+    friends: [],
   },
   {
     toJSON: {
